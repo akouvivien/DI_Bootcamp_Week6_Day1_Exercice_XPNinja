1d2637573ba80218b6a8c65115344e9a96e1b598
akouvivien <akouvivien@gmail.com>
2023-02-01 17:41:37 +0000

    initial commit

---
+++ Exercice_1/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+
+    int age =27;
+    double weight = 80;
+    boolean isStudent = true;
+    char gender= 'M';
+    String name ="AKOU";
+
+    public void displayInfo() {
+        System.out.println("age"+age+"weight"+weight+"isStudent"+isStudent+"gender"+gender+"name"+name);     
+    }
+    public static void main(String[] args){
+
+        // App.displayInfo();
+    }
+}
