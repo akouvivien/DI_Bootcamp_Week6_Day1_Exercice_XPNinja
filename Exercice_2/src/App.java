1b3078c127f37dc92cebc877f17c76b431977a16
akouvivien <akouvivien@gmail.com>
2023-02-03 01:51:01 +0000

    add exo 2

---
+++ Exercice_2/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int age = 25;
+        double taille = 1.25;
+        char sexe = 'M';
+        boolean isTrue = true ;
+        
+        System.out.println(" age "+age+" taille "+taille+" sexe "+sexe+" isTrue "+isTrue);
+        System.out.println();
+        age = 30;   
+        taille = 2.25;
+        sexe = 'F';    
+        isTrue = false ;
+        System.out.println(" age "+age+"taille "+taille+" sexe "+sexe+" isTrue "+isTrue);
+    }
+}
